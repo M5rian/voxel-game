48e48e6305eac1909b119b14c365a3d8f9758dd6
marian <elias-marian@web.de>
2023-04-03 20:42:57 +0200

    Added base code including a moveable camera, block and block rendering

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use tokio::runtime::Builder;
+ 
+fn main() {
+    env_logger::init();
+    let runtime = Builder::new_current_thread().build().unwrap();
+    runtime.block_on(tutorial12_camera::run());
+}
